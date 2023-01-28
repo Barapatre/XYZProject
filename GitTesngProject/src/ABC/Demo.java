862c26fc8264533a36ef99880905a18f61aeace0
Siddhesh <sbarapatre7494@gmail.com>
2023-01-28 12:35:46 +0530

    By Ujwala

---
+++ GitTesngProject/src/ABC/Demo.java
@@ -0,0 +1,15 @@
+package ABC;
+
+public class Demo {
+	
+	public void m1()
+	{
+		System.out.println("Welcome Selenium");
+	}
+
+	public static void main(String[] args) {
+	System.out.println("Hello Java");
+	System.out.println("GitProject");
+	}
+
+}
